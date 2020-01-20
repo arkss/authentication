d3c242b67661cfcd041a5e6419e1778c70d5e358
minsung <rkdalstjd9@naver.com>
2020-01-21 00:44:25 +0900

    admin page 커스텀

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Profile, Salt
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['username', 'name', 'gender', 'email']
+    search_fields = ['username']
+    
 admin.site.register(Salt)
